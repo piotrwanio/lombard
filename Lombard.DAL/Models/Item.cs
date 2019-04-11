0900998008a5e858ab303e1133cc432df806dd06
Piotr <piotrwanio@gmail.com>
2019-04-11 15:11:52 +0200

    added repos, added api

---
+++ Lombard.DAL/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.BLL
+{
+    public class Item
+    {
+        public Item(int itemID, string name, decimal purchasePrice, double quantity, string unit)
+        {
+            ItemID = itemID;
+            Name = name;
+            PurchasePrice = purchasePrice;
+            Quantity = quantity;
+            Unit = unit;
+        }
+
+        public int ItemID { get; set; }
+        public string Name { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public decimal SellingPrice { get; set; }
+    }
+}
