777a8e466ae866ac9be3ef4ba453a24569761ce1
Patryk Moga <46868740+PatrykMoga@users.noreply.github.com>
2019-04-11 12:05:34 +0200

    creating item model

---
+++ Lombard.BLL/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.BLL
+{
+    public class Item
+    {
+        public Item(int itemID, string name, decimal purchasePrice, double quantity, string unit)
+        {
+            ItemID = itemID;
+            Name = name;
+            PurchasePrice = purchasePrice;
+            Quantity = quantity;
+            Unit = unit;
+        }
+
+        public int ItemID { get; private set; }
+        public string Name { get; private set; }
+        public decimal PurchasePrice { get; private set; }
+        public double Quantity { get; private set; }
+        public string Unit { get; private set; }
+
+        public decimal SellPrice => CalculateSellingPrice();
+
+        public void UpdateQuantity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateSellingPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateProfit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lombard.BLL/Transactions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.BLL
+{
+    class Transactions
+    {
+    }
+}
