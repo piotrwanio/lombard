cae9d074fa1acb0123e50316fe50a5721497abe3
Piotr <piotrwanio@gmail.com>
2019-04-11 13:11:12 +0200

    added report

---
+++ Lombard.BLL/Models/Report.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.BLL.Models
+{
+    public class Report
+    {
+        public Report(int reportId, double totalRotation, double totalProfit, int stock)
+        {
+            ReportId = reportId;
+            TotalRotation = totalRotation;
+            TotalProfit = totalProfit;
+            Stock = stock;
+        }
+
+        private int ReportId { get; }
+        private double TotalRotation { get; }
+        private double TotalProfit { get; }
+        private int Stock { get; }
+        
+    }
+}
