ec0f04ca3e581e7569b04b76b71074fd67250395
Patryk Moga <46868740+PatrykMoga@users.noreply.github.com>
2019-04-11 14:01:13 +0200

    item logic + tests

--- Lombard.BLL.Tests/ItemTests.cs
+++ Lombard.BLL.Tests/ItemTests.cs
@@ -17,5 +17,46 @@ namespace Lombard.BLL.Tests
             item.UpdateQuantity(quantity);
             Assert.That(item.Quantity, Is.EqualTo(quantity));
         }
+
+
+        [Test]
+        public void SetSellingPrice_EmployeeSetingItemSellingPrice_SellingPriceIsSet()
+        {
+            var item = new Item(45, "Ryż", 450M, 4, "KG");
+            var expected = 560M;
+            item.SetSellingPrice(expected);
+            Assert.That(item.SellingPrice, Is.EqualTo(expected));
+        }
+
+        [TestCase(589)]
+        [TestCase(967)]
+        [TestCase(823)]
+        public void SetSellingPrice_EmployeesSetingItemSellingPrice_SellingPriceIsSet(decimal sellingPrice)
+        {
+            var item = new Item(45, "Ryż", 450M, 4, "KG");
+            item.SetSellingPrice(sellingPrice);
+            Assert.That(item.SellingPrice, Is.EqualTo(sellingPrice));
+        }
+
+        [Test]
+        public void CalculateProfit_WhenCalled_ReturnsProfit()
+        {
+            var item = new Item(45, "Ryż", 450M, 4, "KG");
+            item.SetSellingPrice(455);
+            var expected = 5M;
+            var actual = item.CalculateProfit();
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase(560,110)]
+        [TestCase(888,438)]
+        [TestCase(900,450)]
+        public void CalculateProfit_WhenCalled_ReturnsProfits(decimal sellingPrice, decimal profit)
+        {
+            var item = new Item(45, "Ryż", 450M, 4, "KG");
+            item.SetSellingPrice(sellingPrice);
+            var actual = item.CalculateProfit();
+            Assert.That(actual, Is.EqualTo(profit));
+        }
     }
 }
