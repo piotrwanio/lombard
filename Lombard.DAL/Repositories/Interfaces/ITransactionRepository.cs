f0c866e35ec695fac39de64be05edf565fa80733
Karolina <karolina.dorota.lichon@gmail.com>
2019-04-12 20:40:48 +0200

    Stworzenie interfejsów

--- Lombard.DAL/Repositories/Interfaces/ITransactionRepository.cs
+++ Lombard.DAL/Repositories/Interfaces/ITransactionRepository.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Lombard.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+
 
 namespace Lombard.DAL.Repositories.Interfaces
 {
     public interface ITransactionRepository
     {
+        Transaction GetTransaction(int id);
+        bool DeleteTransaction(int id);
+        bool AddTransaction(Transaction transaction);
+        bool UpdateTransaction(Transaction transaction);
+
+        List<Transaction> GetAllTransactions();
+        List<Transaction> GetTransactionsByType(TransactionType type);
+        List<Transaction> GetTransactionsByClient(Customer customer);
+        List<Transaction> GetTransactionByEmployee(Employee employee);
+
+
+
     }
 }
