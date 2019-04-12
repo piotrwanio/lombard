61f8603ab2fddd12a0849433e9712e4e601c0560
Piotr <piotrwanio@gmail.com>
2019-04-13 01:31:15 +0200

    dodanie akcji getitembyid w kontrolerze product

--- Lombard.API/Controllers/ProductController.cs
+++ Lombard.API/Controllers/ProductController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Lombard.BLL.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Lombard.DAL.Models;
+using Lombard.DAL.Repositories.Interfaces;
+using Lombard.DAL.Repositories.Implementations;
 
 namespace Lombard.API.Controllers
 {
@@ -12,5 +15,18 @@ namespace Lombard.API.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private IItemRepository _itemRepository;
+
+        public ProductController(IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+
+        [HttpGet("get/{id}")]
+        public ActionResult<Item> GetItemById(int id)
+        {
+            Item item = _itemRepository.GetItemById(id);
+            return item;
+        }
     }
 }
