b902a091aca59f386d071aa1e3a5bc6fc23136f1
Karolina <karolina.dorota.lichon@gmail.com>
2019-04-12 21:39:06 +0200

    opracowane Employee

---
+++ Lombard.DAL.Tests/EmplyeeRepositoryTests.cs
@@ -0,0 +1,79 @@
+﻿using Lombard.DAL.Models;
+using Lombard.DAL.Repositories.Implementations;
+using Lombard.DAL.Repositories.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lombard.DAL.Tests
+{
+    [TestFixture]
+    class EmplyeeRepositoryTests
+    {
+        IEmployeeRepository employeeRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+
+        [Test]
+        public void GetEmployee_ValidTest()
+        {
+          Employee  result = employeeRepository.GetEmployee(1);
+          Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetEmployee_NotValidTest()
+        {
+            Employee result = employeeRepository.GetEmployee(-1);
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void DeleteEmployee_Succesfull()
+        {
+            bool result = employeeRepository.DeleteEmplyee(1);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void DeleteEmployee_NotSuccesfull()
+        {
+            bool result = employeeRepository.DeleteEmplyee(-1);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void AddEmployee_Succesfull()
+        {
+            bool result = employeeRepository.AddEmployee(new Employee());
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void AddEmployee_NotSuccesfull()
+        {
+            bool result = employeeRepository.AddEmployee(null);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void UpdateEmployee_Succesfull()
+        {
+            bool result = employeeRepository.UpdateEmployee(new Employee());
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void UpdateEmployee_NotSuccesfull()
+        {
+            bool result = employeeRepository.UpdateEmployee(null);
+            Assert.IsFalse(result);
+        }
+    }
+}
--- Lombard.DAL/Repositories/Implementations/EmployeeRepository.cs
+++ Lombard.DAL/Repositories/Implementations/EmployeeRepository.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using Lombard.DAL.Models;
+using Lombard.DAL.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lombard.DAL.Repositories.Implementations
 {
-    public class EmployeeRepository
+    public class EmployeeRepository : IEmployeeRepository
     {
+        public bool AddEmployee(Employee employee)
+        {
+            if(employee == null) return false;
+            return true;
+        }
+
+        public bool DeleteEmplyee(int id)
+        {
+            if (id <= 0) return false;
+            return true;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+           if(id > 0)
+            {
+                return new Employee();
+            }
+            return null;
+        }
+
+        public bool UpdateEmployee(Employee employee)
+        {
+            if (employee == null) return false;
+            return true;
+        }
     }
 }
